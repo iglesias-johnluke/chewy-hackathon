0cce0303638474df88e17a9c7d20d30f941b5adf
iglesias-johnluke <iglesiasjohnluke@gmail.com>
2022-01-17 10:15:12 -0500

    finish katerra merge

--- dogProfile/profileScript.js
+++ dogProfile/profileScript.js
@@ -5,3 +5,16 @@ var profileImage = document.querySelector('.dogtag img')
 
 profileHeader.textContent = dogName
 profileImage.src = '../images/'.concat(dogName).concat('.png')
+
+
+document.getElementById("size").innerHTML = "size";
+document.getElementById("shedding").innerHTML = "shedding";
+document.getElementById("heathIssues").innerHTML = "heathIssues";
+document.getElementById("loud").innerHTML = "size";
+document.getElementById("hypoallergenic").innerHTML = "size";
+document.getElementById("yard").innerHTML = "size";
+document.getElementById("goodKids").innerHTML = "size";
+document.getElementById("goodOthers").innerHTML = "size";
+document.getElementById("protective").innerHTML = "size";
+
+
