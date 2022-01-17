b9bdc94d1226ea6df4ed0aab8ea6a314d68f4927
Katerra Stamps <katerrastamps@gmail.com>
2022-01-17 08:52:01 -0600

    readd pictures

--- dogProfile/dogProfile.js
+++ dogProfile/dogProfile.js
@@ -1,2 +1,12 @@
+
 document.getElementById("size").innerHTML = "size";
+document.getElementById("shedding").innerHTML = "shedding";
+document.getElementById("heathIssues").innerHTML = "heathIssues";
+document.getElementById("loud").innerHTML = "size";
+document.getElementById("hypoallergenic").innerHTML = "size";
+document.getElementById("yard").innerHTML = "size";
+document.getElementById("goodKids").innerHTML = "size";
+document.getElementById("goodOthers").innerHTML = "size";
+document.getElementById("protective").innerHTML = "size";
+
 console.log("testing");
